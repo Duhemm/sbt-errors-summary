d4d38b33079c4fd80d83ca762d8bd00cdac38411
Martin Duhem <martin.duhem@gmail.com>
2017-05-06 17:53:37 +0200

    `compilerReporter` must have a `Configuration`

--- src/main/scala/sbt/errorssummary/Plugin.scala
+++ src/main/scala/sbt/errorssummary/Plugin.scala
@@ -9,12 +9,14 @@ object Plugin extends AutoPlugin {
   override def trigger  = allRequirements
 
   override def projectSettings: Seq[Setting[_]] =
-    Seq(
-      compilerReporter in compile := {
-        val parent    = (compilerReporter in compile).value
-        val logger    = streams.value.log
-        val sourceDir = sourceDirectory.value.getAbsolutePath
-        Some(new ConciseReporter(logger, sourceDir, parent))
-      }
-    )
+    inConfig(Compile)(reporterSettings) ++
+      inConfig(Test)(reporterSettings)
+
+  private val reporterSettings =
+    compilerReporter in compile := {
+      val parent    = (compilerReporter in compile).value
+      val logger    = streams.value.log
+      val sourceDir = sourceDirectory.value.getAbsolutePath
+      Some(new ConciseReporter(logger, sourceDir, parent))
+    }
 }
