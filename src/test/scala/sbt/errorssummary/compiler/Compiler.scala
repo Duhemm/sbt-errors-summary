b1b0504f2c538a02dfa9614bf9c12ec013b0dd88
Martin Duhem <martin.duhem@gmail.com>
2017-06-30 09:05:18 +0200

    Initial tests

---
+++ src/test/scala/sbt/errorssummary/compiler/Compiler.scala
@@ -0,0 +1,107 @@
+package sbt
+package errorssummary
+package compiler
+
+import xsbti.{Maybe, Reporter}
+
+import scala.reflect.internal.util.{BatchSourceFile, NoFile}
+import scala.reflect.internal.util.Position
+
+import scala.tools.nsc.util.CommandLineParser
+import scala.tools.nsc.{CompilerCommand, Global, Settings}
+import scala.tools.nsc.io.VirtualDirectory
+import scala.tools.nsc.reporters.{AbstractReporter, Reporter => NSCReporter}
+
+/**
+ * Helper object to compile code snippets to a virtual directory.
+ */
+class Compiler(reporter: Reporter) {
+
+  case class CompilationFailed(msg: String) extends Exception(msg)
+
+  /**
+   * Compiles the given code, passing the given options to the compiler
+   */
+  def compile(code: String, options: String*): Unit = {
+    val (wrappedCode, posFn) = wrap(code)
+
+    val cpOpt       = Seq("-cp", sys.props("sbt.class.directory"))
+    val nscReporter = xsbt.WrappedReporter(reporter, posFn)
+    val global      = getCompiler(nscReporter, options = cpOpt ++ options: _*)
+
+    import global._
+    val source = new BatchSourceFile(NoFile, wrappedCode)
+    val run    = new Run
+    run.compileSources(source :: Nil)
+  }
+
+  private def reportError(error: String) = throw CompilationFailed(error)
+
+  /**
+   * Returns an instance of `Global` configured according to the given options.
+   */
+  private def getCompiler(reporter: NSCReporter, options: String*): Global = {
+    // I don't really know how I can reset the compiler after a run, nor what else
+    // should also be reset, so for now this method creates new instances of everything,
+    // which is not so cool.
+    val arguments = CommandLineParser.tokenize(options mkString " ")
+    val command   = new CompilerCommand(arguments.toList, reportError _)
+    val outputDir = new VirtualDirectory("(memory)", None)
+    command.settings.outputDirs setSingleOutput outputDir
+
+    new Global(command.settings, reporter)
+  }
+
+  /**
+   * Wraps a code snippet in an empty class (scalac refuses to compile isolated code snippets.)
+   * Doesn't modify the code if it doesn't need to be wrapped
+   */
+  private def wrap(code: String): (String, xsbti.Position => xsbti.Position) = {
+    if (code startsWith "import") {
+      val Array(imp, rest @ _ *) = code.split("\n")
+      val (wrapped, fn)          = wrap(rest.mkString("\n"))
+      (imp + "\n" + wrapped, fn)
+    } else if (code.startsWith("package") || code.startsWith("class") || code
+                 .startsWith("object")) {
+      (code, identity)
+    } else
+      (s"""class Compilation {
+          |$code
+          |}""".stripMargin,
+       mapPos(_ - 1, identity))
+  }
+
+  /**
+   * Creates a position mapping function
+   */
+  private def mapPos(lineFn: Int => Int,
+                     colFn: Int => Int): xsbti.Position => xsbti.Position =
+    orig =>
+      new xsbti.Position {
+        import scala.language.implicitConversions
+        private implicit def o2m[T](o: Option[T]): Maybe[T] =
+          o.map(Maybe.just(_)).getOrElse(Maybe.nothing[T])
+
+        def line(): xsbti.Maybe[Integer] =
+          orig.line().map(l => lineFn(l.toInt): Integer)
+
+        def lineContent(): String =
+          orig.lineContent()
+
+        def offset(): xsbti.Maybe[Integer] =
+          orig.offset().map(c => colFn(c.toInt): Integer)
+
+        def pointer(): xsbti.Maybe[Integer] =
+          orig.pointer().map(c => colFn(c.toInt): Integer)
+
+        def pointerSpace(): xsbti.Maybe[String] =
+          orig.pointerSpace().map(s => " " * colFn(s.length))
+
+        def sourceFile(): xsbti.Maybe[java.io.File] =
+          orig.sourceFile()
+
+        def sourcePath(): xsbti.Maybe[String] =
+          orig.sourcePath()
+    }
+
+}
---
+++ src/test/scala/xsbt/WrappedReporter.scala
@@ -0,0 +1,36 @@
+package xsbt
+
+import xsbti.{Position, Reporter, Severity}
+
+object WrappedReporter {
+  def apply(reporter: Reporter, posTransform: Position => Position)
+    : scala.tools.nsc.reporters.Reporter = {
+    val transformedReporter =
+      new TransformedPositionsReporter(reporter, posTransform)
+    new DelegatingReporter(true, false, transformedReporter)
+  }
+}
+
+private class TransformedPositionsReporter(reporter: Reporter,
+                                           posTransform: Position => Position)
+    extends Reporter {
+  def comment(pos: Position, msg: String): Unit =
+    reporter.comment(posTransform(pos), msg)
+
+  def hasErrors(): Boolean =
+    reporter.hasErrors()
+  def hasWarnings(): Boolean =
+    reporter.hasWarnings()
+
+  def log(pos: Position, msg: String, severity: Severity): Unit =
+    reporter.log(posTransform(pos), msg, severity)
+
+  def printSummary(): Unit =
+    reporter.printSummary()
+
+  def problems(): Array[xsbti.Problem] =
+    reporter.problems()
+
+  def reset(): Unit =
+    reporter.reset()
+}
